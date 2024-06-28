52992990e5d35ca572f1c915e7c85e3a244bbcdc
Ahsam Aslam <47176342+ahsamaslam@users.noreply.github.com>
2024-06-28 19:57:37 +0500

    direct to git
    direct to git description

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!.... I am testing git this is my first commit");
+Console.WriteLine("Hello, World!.... I directly commiting git");
